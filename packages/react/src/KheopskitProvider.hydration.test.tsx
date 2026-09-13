a27f716071105da07c9ea789304b54ba8b52542e
0xKheops <26880866+0xKheops@users.noreply.github.com>
2026-09-13 13:54:47 +0900

    🔧 Reference jest-dom vitest types

--- packages/react/src/KheopskitProvider.hydration.test.tsx
+++ packages/react/src/KheopskitProvider.hydration.test.tsx
@@ -1,4 +1,4 @@
-/// <reference types="@testing-library/jest-dom" />
+/// <reference types="@testing-library/jest-dom/vitest" />
 import { ethereum } from "@kheopskit/core/ethereum";
 import { polkadot } from "@kheopskit/core/polkadot";
 import { solana } from "@kheopskit/core/solana";
